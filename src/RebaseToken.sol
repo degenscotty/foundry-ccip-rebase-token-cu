a05a1ff8cdbd3a013f1521a82a2152e16e27b572
degenscotty <scottvermast@gmail.com>
2025-05-12 11:42:51 +0200

    edit: RebaseToken.sol

--- src/RebaseToken.sol
+++ src/RebaseToken.sol
@@ -1,6 +1,27 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
+// Layout of Contract:
+// version
+// imports
+// interfaces, libraries, contracts
+// errors
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 /**
@@ -13,9 +34,33 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
  *      Please do not use in production.
  */
 contract RebaseToken is ERC20 {
+    /*//////////////////////////////////////////////////////////////
+                                 ERRORS
+    //////////////////////////////////////////////////////////////*/
+    error RebaseToken__InterestRateCanOnlyDecrease(uint256 interestRate, uint256 newInterestRate);
+
+    /*//////////////////////////////////////////////////////////////
+                            STATE VARIABLES
+    //////////////////////////////////////////////////////////////*/
     uint256 private s_interestRate = 5e10;
 
+    /*//////////////////////////////////////////////////////////////
+                               FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
     constructor() ERC20("RebaseToken", "RBT") {}
 
-    function setInterestRate(uint256 _newInterestRate) external {}
+    function setInterestRate(uint256 _newInterestRate) external {
+        if (_newInterestRate < s_interestRate) {
+            revert RebaseToken__InterestRateCanOnlyDecrease(s_interestRate, _newInterestRate);
+        }
+
+        s_interestRate = _newInterestRate;
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                                GETTERS
+    //////////////////////////////////////////////////////////////*/
+    function getInterestRate() external view returns (uint256) {
+        return s_interestRate;
+    }
 }
